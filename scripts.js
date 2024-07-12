d182985dfcd992d16668a3ddd8db4fcc3648d39e
Keenan Griffin <Kwgryphin@gmail.com>
2024-07-12 10:55:04 -0400

    index and event changes. Added menu link

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
 console.log("connected.");
-
-const pTag = document.getElementById("first");
-
-console.log(pTag.innerHTML);
-
-function changeText() {
-    const pTag = document.getElementById("first");
-
-    pTag.innerText = "This is cooler";
-}
